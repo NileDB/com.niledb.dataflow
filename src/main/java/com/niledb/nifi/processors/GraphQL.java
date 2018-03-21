9639cd93036555d663b3358e8333a1afc0f5e0f5
Paco Hernández Gómez <paco@hernandezgomez.com>
2018-03-21 18:43:37 +0100

    Demo done.

--- src/main/java/com/niledb/nifi/processors/GraphQL.java
+++ src/main/java/com/niledb/nifi/processors/GraphQL.java
@@ -43,7 +43,6 @@ import org.apache.nifi.processor.util.StandardValidators;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientOptions;
-import io.vertx.core.http.HttpClientRequest;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 
