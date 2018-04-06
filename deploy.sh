2e5f21aad8eef5fe4593efff6eeeabc489353504
Paco Hernández Gómez <paco@paco-thinkpad>
2018-04-06 14:35:57 +0200

    Commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gradle clean build
-docker cp build/libs/com.niledb.nifi-0.8.0.nar nifi:/opt/nifi/nifi-1.5.0/lib
-docker restart nifi
-#docker logs -f nifi
+sudo docker cp build/libs/com.niledb.nifi-0.8.0.nar nifi:/opt/nifi/nifi-1.5.0/lib
+sudo docker restart nifi
+#sudo docker logs -f nifi
